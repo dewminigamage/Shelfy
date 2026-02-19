2c8d39a60f88929d94bf62d81bc2f14405c1aaa7
Dewmini Gamage <pgdgamage2002@gmail.com>
2026-02-19 19:31:23 +0530

    changes

--- BookAPI/obj/Debug/net8.0/BookAPI.AssemblyInfo.cs
+++ BookAPI/obj/Debug/net8.0/BookAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04f87894e19389bcd8772378202878f334fc2839")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
